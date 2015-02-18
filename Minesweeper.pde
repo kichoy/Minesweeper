95cc8c5af41ed8af10d8d8532883e3c9ac4b5229
kichoy <kichoy@697-334-Erebor.sf.k12.internal>
2015-02-18 10:49:38 -0800

    countbombs func not working - out of bounds error

--- Minesweeper.pde
+++ Minesweeper.pde
@@ -19,9 +19,10 @@ public void setup ()
 		for (int c = 0; c < NUM_COLS; c++)
 		{
 			buttons[r][c] = new MSButton(r, c);
+			println(r, c);
+			// println("buttons[r][c].countBombs(): "+buttons[r][c].countBombs(r,c));
 		}
 	}
-
 
 	bombs = new ArrayList<MSButton>();
 	setBombs();
@@ -123,20 +124,36 @@ public class MSButton
 
 	public boolean isValid(int r, int c) // if given coord is a valid square on grid
 	{
-		if ((r >= 0 && r <= NUM_ROWS) && (c >= 0 && r <= NUM_COLS)) 
+		if ((r >= 0 && r < NUM_ROWS) && (c >= 0 && c < NUM_COLS)) 
 		{
 			return true;
 		}
 		return false;
 	}
 
-	public int countBombs(int row, int col)
+	public int countBombs(int row, int col) //counts number of bombs touching square
 	{
 		int numBombs = 0;
-		if (isValid(row, col) && bombs.contains(buttons[row][col]) 
+		for (int j = -1; j <= 1; j++)
 		{
-			numBombs++;
+			for (int i = -1; i <= 1; i++)
+			{
+				if ((isValid(row+j, col+i) == true) && (j != 0) && (i != 0))
+				{
+					// println("(isValid(row+j, col+i): "+(isValid(row+j, col+i)));
+					// println("(row+j): "+(row+j));
+					// println("(col+i): "+(col+i));
+
+					// println("prints the button: "+buttons[row+j][col+i]);
+					
+					// if (bombs.contains(buttons[row+j][col+i]))
+					// {
+					// 	numBombs++;
+					// }
+				}
+			}	
 		}
+
 		return numBombs;
 	}
 }
--- build-tmp/source/Minesweeper.java
+++ build-tmp/source/Minesweeper.java
@@ -14,3 +14,173 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
+public class Minesweeper extends PApplet {
+
+
+
+
+int NUM_ROWS = 20; int NUM_COLS = 20;
+private MSButton[][] buttons; //2d array of minesweeper buttons
+private ArrayList <MSButton> bombs; //ArrayList of just the minesweeper buttons that are mined
+
+public void setup ()
+{
+	size(400, 400);
+	textAlign(CENTER,CENTER);
+
+	// make the manager
+	Interactive.make( this );
+	
+	buttons = new MSButton[NUM_ROWS][NUM_COLS];
+	for (int r = 0; r < NUM_ROWS; r++)
+	{
+		for (int c = 0; c < NUM_COLS; c++)
+		{
+			buttons[r][c] = new MSButton(r, c);
+			println(r, c);
+			// println("buttons[r][c].countBombs(): "+buttons[r][c].countBombs(r,c));
+		}
+	}
+
+	bombs = new ArrayList<MSButton>();
+	setBombs();
+}
+
+public void setBombs()
+{
+	int numBombs = 10;
+	for (int i = 0; i < numBombs; i++) 
+	{
+		int row = (int)(Math.random()*NUM_ROWS);
+		int col = (int)(Math.random()*NUM_COLS);
+		if (! bombs.contains(buttons[row][col])) //if there isn't a bomb at that spot, add a bomb there 
+		{
+			bombs.add(buttons[row][col]);
+		}
+	}
+}
+
+public void draw ()
+{
+	background( 0 );
+	if(isWon())
+		displayWinningMessage();
+}
+
+public boolean isWon()
+{
+		//your code here
+		return false;
+}
+
+public void displayLosingMessage()
+{
+		//your code here
+}
+
+public void displayWinningMessage()
+{
+		//your code here
+}
+
+public class MSButton
+{
+	private int r, c;
+	private float x,y, width, height;
+	private boolean clicked, marked;
+	private String label;
+
+	public MSButton ( int rr, int cc )
+	{
+		width = 400/NUM_COLS;
+		height = 400/NUM_ROWS;
+		r = rr;
+		c = cc; 
+		x = c*width;
+		y = r*height;
+		label = "";
+		marked = clicked = false;
+		Interactive.add( this ); // register it with the manager
+	}
+
+	public boolean isMarked()
+	{
+		return marked;
+	}
+
+	public boolean isClicked()
+	{
+		return clicked;
+	}
+	// called by manager
+	
+	public void mousePressed () 
+	{
+		clicked = true;
+		//your code here
+	}
+
+	public void draw () 
+	{
+		if (marked)
+			fill(0);
+		else if( clicked && bombs.contains(this) ) 
+			fill(255,0,0);
+		else if(clicked)
+			fill( 200 );
+		else 
+			fill( 100 );
+			rect(x, y, width, height);
+			fill(0);
+			text(label,x+width/2,y+height/2);
+	}
+
+	public void setLabel(String newLabel)
+	{
+		label = newLabel;
+	}
+
+	public boolean isValid(int r, int c) // if given coord is a valid square on grid
+	{
+		if ((r >= 0 && r < NUM_ROWS) && (c >= 0 && c < NUM_COLS)) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public int countBombs(int row, int col) //counts number of bombs touching square
+	{
+		int numBombs = 0;
+		for (int j = -1; j <= 1; j++)
+		{
+			for (int i = -1; i <= 1; i++)
+			{
+				if ((isValid(row+j, col+i) == true) && (j != 0) && (i != 0))
+				{
+					// println("(isValid(row+j, col+i): "+(isValid(row+j, col+i)));
+					// println("(row+j): "+(row+j));
+					// println("(col+i): "+(col+i));
+
+					// println("prints the button: "+buttons[row+j][col+i]);
+					
+					// if (bombs.contains(buttons[row+j][col+i]))
+					// {
+					// 	numBombs++;
+					// }
+				}
+			}	
+		}
+
+		return numBombs;
+	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
